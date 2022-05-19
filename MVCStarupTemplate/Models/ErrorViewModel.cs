76634378a87daf7b4d4684528b944cb3deb7bb20
biserb <butoff@abv.bg>
2022-05-19 20:20:24 +0300

    First commit

---
+++ MVCStarupTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCStarupTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
