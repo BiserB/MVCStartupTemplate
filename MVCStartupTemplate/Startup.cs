1614efc4221a668aeffe66ae16087a3bbf89fcc2
biserb <butoff@abv.bg>
2022-05-20 13:40:16 +0300

    Add Startup. Fix project name

---
+++ MVCStartupTemplate/Startup.cs
@@ -0,0 +1,36 @@
+﻿namespace MVCStarupTemplate
+{
+    public class Startup
+    {
+        private ConfigurationManager configuration;
+
+        public Startup(ConfigurationManager configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        }
+    }
+}
